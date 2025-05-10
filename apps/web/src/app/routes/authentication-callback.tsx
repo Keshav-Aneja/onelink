71548f9b4ec42123f7ba011d67a7e716ffdbced4
Keshav-Aneja <keshav.aneja09@gmail.com>
2025-05-11 02:18:11 +0530

    debug: token setup 5

--- apps/web/src/app/routes/authentication-callback.tsx
+++ apps/web/src/app/routes/authentication-callback.tsx
@@ -1,20 +1,18 @@
-// import { paths } from "@config/paths";
+import { paths } from "@config/paths";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 const AuthenticationCallback = () => {
   const locationURL = useLocation();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleCookie = () => {
     const queryParams = new URLSearchParams(locationURL.search);
     const token = queryParams.get("token");
     if (!token || token.length == 0) {
-      console.log("TOKEN", token);
+      navigate(paths.landing.path);
       return;
-      // navigate(paths.landing.path);
-      // return;
     }
     const existingToken = Cookies.get("connect.sid");
     if (existingToken) return;
@@ -27,19 +25,19 @@ const AuthenticationCallback = () => {
     });
   };
 
-  // const handleRedirect = () => {
-  //   const queryParams = new URLSearchParams(locationURL.search);
-  //   const redirectPath = queryParams.get("redirectTo");
-  //   if (redirectPath && redirectPath.length > 0) {
-  //     navigate(decodeURIComponent(redirectPath));
-  //     return;
-  //   }
-  //   navigate(paths.landing.path);
-  // };
+  const handleRedirect = () => {
+    const queryParams = new URLSearchParams(locationURL.search);
+    const redirectPath = queryParams.get("redirectTo");
+    if (redirectPath && redirectPath.length > 0) {
+      navigate(decodeURIComponent(redirectPath));
+      return;
+    }
+    navigate(paths.landing.path);
+  };
 
   useEffect(() => {
     handleCookie();
-    // handleRedirect();
+    handleRedirect();
   }, []);
   return <div>AuthenticationCallback</div>;
 };
