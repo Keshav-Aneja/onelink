3b2ff976eb30e4c258fdb5b41c14e149ff6ba5fa
Keshav-Aneja <keshav.aneja09@gmail.com>
2025-05-11 02:11:21 +0530

    debug: token setup

--- apps/web/src/app/routes/authentication-callback.tsx
+++ apps/web/src/app/routes/authentication-callback.tsx
@@ -11,8 +11,10 @@ const AuthenticationCallback = () => {
     const queryParams = new URLSearchParams(locationURL.search);
     const token = queryParams.get("token");
     if (!token || token.length == 0) {
-      navigate(paths.landing.path);
+      console.log("TOKEN", token);
       return;
+      // navigate(paths.landing.path);
+      // return;
     }
     const existingToken = Cookies.get("connect.sid");
     if (existingToken) return;
